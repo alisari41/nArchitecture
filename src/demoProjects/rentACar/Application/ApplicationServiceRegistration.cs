ea28695cbcd464081a07856c60c2b27adbf86dfa
alisari41 <alisari41@outlook.com>
2022-09-04 23:08:10 +0300

    Fluent Validation işlemleri yapıldı.

--- src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
+++ src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.Brands.Rules;
+using Core.Application.Pipelines.Validation;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -21,12 +23,13 @@ namespace Application
 
             services.AddScoped<BrandBusinessRules>(); // Business Kuralları bir kere bellekte durur.
 
-            // services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly()); // Fluent Validation: Bir nesnenin özelliklerinin iş kurallarına dahil etmek için format uygunluğu ile ilgili
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly()); // Fluent Validation: Bir nesnenin özelliklerinin iş kurallarına dahil etmek için format uygunluğu ile ilgili
             // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>)); // Rol Bazlı Yetkilendirme
             // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>)); // Ön Belleğe atma
             // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheRemovingBehavior<,>)); // Ön belleği temizleme
             // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>)); // Loglama  
-            // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
 
             return services;
 
---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommandValidator : AbstractValidator<CreateBrandCommand>
+    {
+        // FluentValidation ile Format Doğrulama işlemleri
+        // Ekeleme işlemleri için ayrı güncelleme işlemleri vs. ler için ayrı doğrulama işlemleri olabileceği için "CreateBrandCommand" ile ekleme işlemleri için yapıldı
+        public CreateBrandCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty(); // Boş geçilemez
+            RuleFor(c => c.Name).MinimumLength(2); // En kısa uzunluk 2 karakter
+        }
+    }
+}
