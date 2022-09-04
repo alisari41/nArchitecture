da1c7fafed8d3fcad5a5127f3f28f78c5a1054b3
alisari41 <alisari41@outlook.com>
2022-09-04 15:59:53 +0300

    GetList Aşamaları Yapıldı.

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Models/BrandListModel.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Brands.Dtos;
+using Core.Persistence.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Models
+{
+    public class BrandListModel : BasePageableModel
+    {
+        // Sayfalama için kullanılan sınıf olarak diyebiliriz. Hem Sayfalama verilerini hemde Dto verilerini kullacağız
+        public IList<BrandListDto> Items { get; set; } // İsimlendirmeler aynı olması gerekirki mapperda bir daha configuraiton yapmamak için 
+    }
+}
--- src/demoProjects/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.Brands.Commands.CreateBrand;
 using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Models;
 using AutoMapper;
+using Core.Persistence.Paging;
 using Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -20,6 +22,7 @@ namespace Application.Features.Brands.Profiles
             // AutoMapper'in Profile Sınıfından gelir Amacı: Neyi Neye maplicez Source:kaynak Destination: Hedef
             CreateMap<Brand, CreatedBrandDto>().ReverseMap(); // ReverseMap() iki türlüde mapleme yapmayı sağlar
             CreateMap<Brand, CreateBrandCommand>().ReverseMap(); // ReverseMap() iki türlüde mapleme yapmayı sağlar
+            CreateMap<IPaginate<Brand>, BrandListModel>().ReverseMap();
         }
     }
 }
