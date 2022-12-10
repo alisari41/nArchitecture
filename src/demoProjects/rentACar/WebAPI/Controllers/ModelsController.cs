36c43ea69732b4cd997f3a1143eb0fae595f7599
alisari41 <alisari41@outlook.com>
2022-12-10 13:56:00 +0300

    WebAPI da GetList() metodu yazılmıştır.

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Models.Queries.GetListModel;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        [HttpGet("get-list")]
+        public async Task<ActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuery getLisModelQuery = new() { PageRequest = pageRequest }; // Bu yeni kullanımdır eski hali aşağıdaki gibidir.
+                                                                                      // GetListBrandQuery getListBrandQuery = new GetListBrandQuery();
+                                                                                      // getListBrandQuery.PageRequest = pageRequest;
+
+            var result = await Mediator.Send(getLisModelQuery);
+            return Ok(result);
+        }
+    }
+}
